848d71210fd808e30cc9b1baf9c4a69d11aa3c4e
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2021-03-20 14:05:55 +0100

    trying to convert to matrix multiplication TF

--- models/TTGauss.py
+++ models/TTGauss.py
@@ -6,7 +6,7 @@ tfm = tf.math
 
 
 class TensorTrainGaussian2D(tf.keras.Model):
-    def __init__(self, K, M=None,seed=None):
+    def __init__(self, K, M=None, seed=None):
         super(TensorTrainGaussian2D, self).__init__()
         self.K = K
         self.M = 2
@@ -14,15 +14,11 @@ class TensorTrainGaussian2D(tf.keras.Model):
         if seed != None:
             tf.random.set_seed(seed)
 
-        # Initialize weights that sum to 1
-        Wk0 = np.ones((self.K)) #/ self.K
-
+        # Initialize weights
+        Wk0 = np.ones((self.K))
         Wk1k0 = np.ones((self.K, self.K))
-        #Wk1k0 = Wk1k0 / np.sum(Wk1k0, axis=0)
-
         Wk2k1 = np.ones((self.K, self.K))
-        #Wk2k1 = Wk2k1 / np.sum(Wk2k1, axis=0)
-        
+
         # Define weights as tf variables
         self.Wk0 = tf.Variable(Wk0, name="Wk0", dtype=tf.dtypes.float32)
         self.Wk1k0 = tf.Variable(Wk1k0, name="Wk1k0", dtype=tf.dtypes.float32)
@@ -48,10 +44,17 @@ class TensorTrainGaussian2D(tf.keras.Model):
             ] for i in range(self.K)
         ]
 
-        self.distributions = [
-            [tfd.Normal(self.mu[i][j], self.sigma[i][j]) for j in range(self.K)] 
-            for i in range(self.K)
-        ]
+        # self.distributions = [
+        #     [tfd.Normal(self.mu[i][j], self.sigma[i][j]) for j in range(self.K)] 
+        #     for i in range(self.K)
+        # ]
+        
+        dist = []
+        for i in range(self.K):
+            for j in range(self.K):
+                dist.append(tfd.Normal(self.mu[i][j], self.sigma[i][j]))
+        self.joint = tfd.JointDistributionSequential(dist)
+
         return None
 
     def call(self, X):
@@ -59,19 +62,31 @@ class TensorTrainGaussian2D(tf.keras.Model):
         Wk0 = tf.nn.softmax(self.Wk0)
         Wk1k0 = tf.nn.softmax(self.Wk1k0, axis=0)
         Wk2k1 = tf.nn.softmax(self.Wk2k1, axis=0)
-        for k0 in range(self.K):
-            mid = tf.zeros((X.shape[0]), dtype=tf.dtypes.float32)
-            for k1 in range(self.K):  
-                temp_mid = Wk1k0[k1, k0] * self.distributions[k1][k0].prob(X[:, 0])
-                inner = tf.zeros((X.shape[0]), dtype=tf.dtypes.float32)
-                for k2 in range(self.K):
-                    inner += Wk2k1[k2, k1] * self.distributions[k2][k1].prob(X[:, 1])
-                
-                mid += temp_mid*inner
-            likelihoods += Wk0[k0] * mid
+
+        d1 = []
+        d2 = []
+        [d1.append(X[:, 0]) for s in range(self.K**2)]
+        [d2.append(X[:, 1]) for s in range(self.K**2)]
+
+        A = Wk1k0
+        B = np.reshape(self.joint.log_prob_parts(d1), (self.K, self.K, -1))
+        C = Wk2k1
+        D = np.reshape(self.joint.log_prob_parts(d2), (self.K, self.K, -1))
         
-        log_likelihoods = tfm.log(likelihoods)
-        return log_likelihoods
+        res1 = tf.multiply(A[:, :, np.newaxis], B)
+        res2 = tf.multiply(C[:, :, np.newaxis], D)
+        res = tf.tensordot(res1, res2, axes=2)
+        z = Wk0.numpy()
+        
+
+        print(z.shape)
+        print(res.shape)
+
+        likelihoods = np.sum(z @ res, axis=1)
+        likelihoods = tf.convert_to_tensor(likelihoods, dtype=tf.float32)
+
+        return tfm.log(likelihoods + np.finfo(np.float64).eps)
+
 
     def normalize_weights(self):
         """ Ensure that weights always sum to 1 """        
@@ -89,5 +104,4 @@ class TensorTrainGaussian2D(tf.keras.Model):
         tvars = self.trainable_variables
         gradients = tape.gradient(loss_value, tvars)
         optimizer.apply_gradients(zip(gradients, tvars))
-        #self.normalize_weights()
         return loss_value

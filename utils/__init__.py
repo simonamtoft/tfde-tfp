1c1d14545d3ba03cb677cd85f619c216730218d4
Rasmus <39836677+RTuxen@users.noreply.github.com>
2021-04-26 10:10:55 +0200

    fixed GMM component counter

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,7 +2,7 @@ from .plotting import plot_contours, plot_density
 from .density_functions import unitTest, plot_density_3d, plot_density_3d_paper, \
     plot_density_2d_paper
 from .CV_functions import CV_1_fold, CV_holdout
-from .helpers import get_fair_Ks
+from .helpers import get_fair_Ks,get_free_params
 
 # From ffjord util functions
 from .ffjord_util_functions import isposint
